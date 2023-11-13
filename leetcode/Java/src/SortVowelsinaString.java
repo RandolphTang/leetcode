e961ae4cefea53af523c737bb63b6892998ceafe
RandolphT <y031125k@gmail.com>
2023-11-13 08:47:59 -0800

    Update 11/13

--- leetcode/Java/src/SortVowelsinaString.java
+++ leetcode/Java/src/SortVowelsinaString.java
@@ -3,19 +3,19 @@ public class SortVowelsinaString {
         List<Character> vowels = new ArrayList<>();
         char[] chars = s.toCharArray();
 
-        for(int i = 0; i < s.length(); i++) {
+        for(char c: chars) {
 
-            char d = Character.toLowerCase(s.charAt(i));
+            char d = Character.toLowerCase(c);
 
             if(d == 'a' || d == 'e' ||d == 'i' || d =='o' ||d == 'u') {
-                vowels.add(d);
+                vowels.add(c);
             }
 
         }
 
         Collections.sort(vowels);
 
-        for(int i = 0, j =0; i < chars.length; i++) {
+        for(int i = 0, j =0; i < chars.length; ++i) {
             char e = Character.toLowerCase(chars[i]);
             if(e == 'a' ||e == 'e' || e =='i' ||e == 'o' || e =='u') {
                 chars[i] = vowels.get(j++);
