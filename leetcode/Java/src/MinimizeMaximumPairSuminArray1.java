2e7778228d9f9a90aed142d70847a793d007768d
RandolphT <y031125k@gmail.com>
2023-11-17 11:48:06 -0800

    Update 11/17

---
+++ leetcode/Java/src/MinimizeMaximumPairSuminArray1.java
@@ -0,0 +1,14 @@
+public class MinimizeMaximumPairSuminArray1 {
+    public int minPairSum(int[] nums) {
+
+        Arrays.sort(nums);
+        int minMax = 0;
+
+        for(int i  = 0; i < nums.length / 2; i++) {
+            minMax = Math.max(minMax,
+                    nums[i] + nums[nums.length - i - 1]);
+        }
+
+        return minMax;
+    }
+}
