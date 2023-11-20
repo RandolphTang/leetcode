9bb14981d3f018c7fc345544aa239ec4c1ac6310
RandolphT <y031125k@gmail.com>
2023-11-19 17:09:26 -0800

    Update 11/19

---
+++ leetcode/Java/src/FrequencyoftheMostFrequentElement.java
@@ -0,0 +1,18 @@
+public class FrequencyoftheMostFrequentElement {
+    public int maxFrequency(int[] nums, int k) {
+        int len = nums.length;
+        long sum = 0;
+        int ans = 0;
+
+        Arrays.sort(nums);
+
+        for(int j = 0, i = 0; i < len; i++) {
+            sum += nums[i];
+            while(sum + k < (long)nums[i] * (i - j + 1))
+                sum -= nums[j++];
+            ans = Math.max(ans, i - j + 1);
+        }
+
+        return ans;
+    }
+}
