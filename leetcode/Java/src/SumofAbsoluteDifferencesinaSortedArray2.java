b3c2ebfc7803d6e84619aa02b02ea1f772126f96
RandolphT <y031125k@gmail.com>
2023-11-25 18:37:21 -0800

    Update 11/25

---
+++ leetcode/Java/src/SumofAbsoluteDifferencesinaSortedArray2.java
@@ -0,0 +1,23 @@
+public class SumofAbsoluteDifferencesinaSortedArray2 {
+    public int[] getSumAbsoluteDifferences(int[] nums) {
+        int sum = 0;
+
+        for(int i  = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+
+        int right = sum;
+        int left = 0;
+        int[] ans = new int[nums.length];
+
+        for(int i  = 0; i < nums.length; i++) {
+            int num = nums[i];
+            right -= num;
+
+            ans[i] = num * i - left + right - num * (nums.length - i - 1);
+
+            left += num;
+        }
+        return ans;
+    }
+}
