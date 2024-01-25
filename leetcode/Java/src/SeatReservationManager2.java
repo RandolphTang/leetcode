2bf3d63982068f862da40e35a4e8c144f2dd8c3f
Randolph Jenkins <tang1125@uw.edu>
2024-01-24 21:20:06 -0800

    1/24

--- leetcode/Java/src/SeatReservationManager2.java
+++ leetcode/Java/src/SeatReservationManager2.java
@@ -5,6 +5,17 @@ public class SeatReservationManager2 {
     //This only used vector, which I guess saves runtime compared to
     //Priority Queue for this problem
 
+    // in this solution, all seats are assumed reserved already (does not affect finding index that
+    // trace true or false inside boolean[]
+    // so when a great number of input, the first if condition block in reserve() helps
+    // trace which index the reserve process has been proceed to.
+    // so when unreserved, any random number unreserved, int unreserved changes property (even though
+    // int, but is more likely a boolean (0 or -1), 0 means continue next finding of empty location
+    // -1 means finding the smallest empty slot to fill seats
+    // therefore, when large input, the first condtion enhance performance
+    // if says the first 1 million is all reserved, it would take so long to 1 million and 1
+    // than first condition helps directly find the next 1 million and 1 slot
+
     private boolean[] seatM;
     private int unreserved;
 
