edd1f5dc12bea3af37f07bea9d414ebf7559839a
Randolph Jenkins <tang1125@uw.edu>
2024-01-27 17:26:36 -0800

    1/27

--- leetcode/Java/src/EliminateMaximumNumberofMonster.java
+++ leetcode/Java/src/EliminateMaximumNumberofMonster.java
@@ -15,7 +15,10 @@ public class EliminateMaximumNumberofMonster {
         //gets the priority of monster will arrive first
 
         for(int i = 0; i < n; i++) {
-            if(i > arrTime[i]) {
+            if(i > arrTime[i]) { // think i as time, says i = 0 (right now), and first
+                                //monster will arrive right now (i = 0) or later
+                                // so we can sure kill first monster
+                                // if the second monster arrived before recharge, the kill is minimum 1
                 return i; // number of monster killed
             }
         }
