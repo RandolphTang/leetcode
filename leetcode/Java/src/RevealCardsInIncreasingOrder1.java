1ba431cca6689543ea42f593d83b317f7d5669df
tang1125@uw.edu <tang1125@uw.edu>
2024-04-09 22:04:48 -0700

    4/9

--- leetcode/Java/src/RevealCardsInIncreasingOrder1.java
+++ leetcode/Java/src/RevealCardsInIncreasingOrder1.java
@@ -1,29 +1,22 @@
 public class RevealCardsInIncreasingOrder1 {
-    // this solution can not handle large cases only 28/33 passed
+
 
     public int[] deckRevealedIncreasing(int[] deck) {
-        int max = 0;
-        int maxLoc = 0;
-        int maxL = 0;
+
         List<Integer> result = new ArrayList<>();
         int[] finalR = new int[deck.length];
 
-        for(int i = 0; i < deck.length; i++){
-            for(int j = 0; j < deck.length; j++){
-                if(deck[j] != 0 && deck[j] > max){
-                    max = deck[j];
-                    maxLoc = j;
-                }
-            }
+        Arrays.sort(deck);
 
-            result.add(0, max);
+        for(int i = 0; i < deck.length; i++){
+
+            result.add(0, deck[deck.length - i - 1]);
+
             if(result.size() >= 3) {
                 result.add(1, result.get(result.size() - 1));
                 result.remove(result.size() - 1);
             }
 
-            max = 0;
-            deck[maxLoc] = 0;
         }
 
         for(int i = 0; i < deck.length; i++) {
