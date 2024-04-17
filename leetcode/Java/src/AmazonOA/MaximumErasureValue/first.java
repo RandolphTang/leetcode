a9ae9185ac8717441bf43fa44bd21913bb0de7b1
tang1125@uw.edu <tang1125@uw.edu>
2024-04-17 15:05:29 -0700

    4/17

---
+++ leetcode/Java/src/AmazonOA/MaximumErasureValue/first.java
@@ -0,0 +1,33 @@
+package AmazonOA.MaximumErasureValue;
+//slower in time complexity
+
+public class first {
+    public int maximumUniqueSubarray(int[] nums) {
+        if(nums.length == 1){
+            return nums[0];
+        }
+
+        Set<Integer> s = new HashSet<>();
+        int maxSum = 0;
+        int currSum = 0;
+        int left = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+
+            while(s.contains(nums[i])){
+
+                int remove = nums[left];
+                s.remove(remove);
+                currSum = currSum - remove;
+                left++;
+            }
+
+            currSum += nums[i];
+            maxSum = Math.max(currSum, maxSum);
+            s.add(nums[i]);
+        }
+
+        return maxSum;
+
+    }
+}
