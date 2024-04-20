0e32f41ae3d64eb285889c56253e11c5463dc86f
tang1125@uw.edu <tang1125@uw.edu>
2024-04-20 12:18:16 -0700

    4/20

---
+++ leetcode/Java/src/AprilWeek2/FindAllGroupsofFarmland/Solution1.java
@@ -0,0 +1,35 @@
+package AprilWeek2.FindAllGroupsofFarmland;
+
+public class Solution1 {
+    public int[][] findFarmland(int[][] land) {
+        List<int[]> ans = new ArrayList<>();
+
+        for(int i = 0; i < land.length;i++){
+            for(int j = 0; j < land[0].length; j++){
+                if(land[i][j] == 1){
+                    int[] cell = new int[]{i, j};
+                    dfs(land, i, j, cell);
+                    ans.add(new int[]{i, j, cell[0], cell[1]});
+                }
+            }
+        }
+
+        return ans.stream().toArray(int[][] :: new);
+    }
+
+    public void dfs(int[][] land, int i, int j, int[] cell) {
+        if(i < 0 || i == land.length || j < 0 || j == land[0].length){
+            return;
+        }
+
+        if(land[i][j] != 1){
+            return;
+        }
+
+        land[i][j] = 2;
+        cell[1] = Math.max(cell[1], j);
+        cell[0] = Math.max(cell[0], i);
+        dfs(land, i + 1, j, cell);
+        dfs(land, i, j + 1, cell);
+    }
+}
