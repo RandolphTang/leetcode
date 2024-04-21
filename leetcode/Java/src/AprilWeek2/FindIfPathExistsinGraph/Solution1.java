cbbe238574b1ad1699f64af51c5316905a63408c
tang1125@uw.edu <tang1125@uw.edu>
2024-04-21 15:08:09 -0700

    4/21

---
+++ leetcode/Java/src/AprilWeek2/FindIfPathExistsinGraph/Solution1.java
@@ -0,0 +1,38 @@
+package AprilWeek2.FindIfPathExistsinGraph;
+
+public class Solution1 {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for(int[] edge: edges){
+            int node1 = edge[0];
+            int node2 = edge[1];
+
+            if(!map.containsKey(node1))map.put(node1, new ArrayList<>());
+            if(!map.containsKey(node2))map.put(node2, new ArrayList<>());
+            map.get(node1).add(node2);
+            map.get(node2).add(node1);
+        }
+
+        int[] visited = new int[n];
+        Queue<Integer> pq = new PriorityQueue<>();
+        pq.offer(source);
+        visited[source] = 1;
+
+        while(!pq.isEmpty()){
+            int next = pq.poll();
+            if(next == destination){
+                return true;
+            }
+            visited[next] = 1;
+            for(int eachNode: map.get(next)){
+                if(visited[eachNode] == 0){
+                    visited[eachNode] = 1;
+                    pq.offer(eachNode);
+                }
+            }
+        }
+
+        return source == destination? true : false;
+    }
+}
---
+++ leetcode/Java/src/AprilWeek2/FindIfPathExistsinGraph/Solution2.java
@@ -0,0 +1,37 @@
+package AprilWeek2.FindIfPathExistsinGraph;
+
+public class Solution2 {
+    private int[] disjointSet;
+
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+
+        disjointSet = new int[n];
+
+        for(int i = 0; i < n; i++){
+            disjointSet[i] = i;
+        }
+
+        for(int[] edge: edges){
+            int node1 = find(edge[0]);
+            int node2 = find(edge[1]);
+            if(node1 != node2){
+                disjointSet[node1] = node2;
+            }
+        }
+
+        return find(source) == find(destination);
+
+
+    }
+
+    public int find(int node){
+        while(disjointSet[node] != node){
+
+            disjointSet[node] = disjointSet[disjointSet[node]];
+            node = disjointSet[node];
+
+        }
+
+        return node;
+    }
+}
