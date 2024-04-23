e11d6aa6a26dc4a246a4fe5dbcc1a1d5d632c829
tang1125@uw.edu <tang1125@uw.edu>
2024-04-22 19:35:06 -0700

    4/22

--- leetcode/Java/src/AprilWeek2/OpenTheLock/Solution1.java
+++ leetcode/Java/src/AprilWeek2/OpenTheLock/Solution1.java
@@ -2,41 +2,41 @@ package AprilWeek2.OpenTheLock;
 
 public class Solution1 {
     public int openLock(String[] deadends, String target) {
-        Set<String> seen = new HashSet<>(Arrays.asList(deadends));
+        HashSet<String> seen = new HashSet<>(Arrays.asList(deadends));
+
         if(seen.contains("0000")) return -1;
         if(target.equals("0000")) return 0;
 
+        Queue<String> possible = new ArrayDeque<>(Arrays.asList("0000"));
         int ans = 0;
-        Queue<String> q = new ArrayDeque<>(Arrays.asList("0000"));
 
-        while(!q.isEmpty()) {
+        while(!possible.isEmpty()){
             ans++;
-            for(int sz = q.size(); sz >0; --sz) {
-                StringBuilder sb = new StringBuilder(q.poll());
-                for(int i = 0; i < 4; ++i) {
-                    final char cache = sb.charAt(i);
-                    sb.setCharAt(i, sb.charAt(i) == '9' ? '0' : (char)(sb.charAt(i) + 1));
+            for(int bfsLevel1 = possible.size(); bfsLevel1 > 0; bfsLevel1--){
+                StringBuilder sb = new StringBuilder(possible.poll());
+                for(int i = 0; i < 4; i++){
+                    final char character = sb.charAt(i);
+                    sb.setCharAt(i, sb.charAt(i) == '9'? '0' : (char)(sb.charAt(i) + 1));
                     String word = sb.toString();
                     if(word.equals(target)) return ans;
-                    if(!seen.contains(word)){
-                        q.offer(word);
+                    if(!seen.contains(word)) {
                         seen.add(word);
+                        possible.offer(word);
                     }
-                    sb.setCharAt(i, cache);
+                    sb.setCharAt(i, character);
 
-                    sb.setCharAt(i, sb.charAt(i) == '0' ? '9' : (char) (sb.charAt(i) - 1));
+
+                    sb.setCharAt(i, sb.charAt(i) == '0'? '9' : (char)(sb.charAt(i) - 1));
                     word = sb.toString();
-                    if (word.equals(target))
-                        return ans;
-                    if (!seen.contains(word)) {
-                        q.offer(word);
+                    if(word.equals(target)) return ans;
+                    if(!seen.contains(word)) {
                         seen.add(word);
+                        possible.offer(word);
                     }
-                    sb.setCharAt(i, cache);
+                    sb.setCharAt(i, character);
                 }
             }
         }
-
-    return -1;
+        return -1;
     }
 }
